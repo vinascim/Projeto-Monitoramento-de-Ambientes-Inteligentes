7e5185b7a3cf57f675c55b6ea1ab6ae66379f97a
Victor Luís <69062237+vinascim@users.noreply.github.com>
2023-09-12 22:29:53 -0300

    Update ProjetoN1.ino

--- ProjetoN1.ino
+++ ProjetoN1.ino
@@ -12,6 +12,138 @@ LiquidCrystal lcd(12, 11, 10, 5, 4, 3, 2);
 
 dht DHT;
 
+int thisChar = 0 ;
+
+//preparando a animação de inicialização
+byte chardino1[8] = {
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B10000,
+  B10000,
+  B11000,
+  B11100
+};
+
+byte chardino2[8] = {
+  B00000,
+  B00001,
+  B00001,
+  B00001,
+  B00001,
+  B00011,
+  B01111,
+  B11111
+};
+byte chardino3[8] = {
+  B11111,
+  B10111,
+  B11111,
+  B11111,
+  B11100,
+  B11111,
+  B11100,
+  B11100
+};
+byte chardino4[8] = {
+  B10000,
+  B11000,
+  B11000,
+  B11000,
+  B00000,
+  B10000,
+  B00000,
+  B00000
+};
+byte chardino5[8] = {
+  B11111,
+  B11111,
+  B01111,
+  B00111,
+  B00011,
+  B00011,
+  B00010,
+  B00011
+};
+byte chardino6[8] = {
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B10110,
+  B00010,
+  B00010,
+  B00011
+};
+
+byte chardino7[8] = {
+  B11111,
+  B11001,
+  B10000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000
+};
+
+
+byte charcabeza1[8] = {
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11100,
+  B11111,
+  B11100,
+  B11100
+};
+
+byte charpieder1[8] = {
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B10110,
+  B00011,
+  B00000,
+  B00000
+};
+
+byte charpieder2[8] = {
+  B11111,
+  B11001,
+  B10000,
+  B00000,
+  B00000,
+  B10000,
+  B00000,
+  B00000
+};
+
+byte charpieiz1[8] = {
+  B11111,
+  B11111,
+  B01111,
+  B00111,
+  B00011,
+  B00011,
+  B00000,
+  B00000
+};
+
+byte charpieiz2[8] = {
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B00110,
+  B10010,
+  B00010,
+  B00011
+};
+
 //declarando as variaveis
 const int pinoDHT11 = A0;
 float mediaTemp = 0;
@@ -21,6 +153,7 @@ int contadorMedia = 0;
 String logEEPROM = "";
 int pinoLed = 7;
 int pinoBuzzer = 8; 
+int contTimer = 0;
 
  
 void setup(){
@@ -32,13 +165,6 @@ void setup(){
   //iniciando o lcd e limpando para garantir que nao apareça nada
   lcd.begin(16,2);              
   lcd.clear();
-  
-  //pegando a data atual pelo rtc
-  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-
-  //printando na segunda linha do lcd uma mensagem de inicio
-  lcd.setCursor(0,1);
-  lcd.print("Analisando...");
 
   //indicando para o arduino se a entrada e output ou input
   pinMode(pinoBuzzer, INPUT); 
@@ -46,12 +172,30 @@ void setup(){
   
   //iniciando o console
   Serial.begin(9600);
+
+ //chamando a função de animação
+  letras();
+  dinoAnimacion();
+
+    //pegando a data atual pelo rtc
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
 }
  
 void loop(){
   
   //pegando a data atual
   DateTime dt = rtc.now(); 
+
+ //contador de tempo para primeira analise
+  contTimer++;
+
+  if(contTimer < 60 ){
+    lcd.setCursor(13, 1);
+    lcd.print(contTimer);
+    lcd.setCursor(15, 1);
+    lcd.print("s");
+  }
+  
                  
   //declarando variaveis
   int sensorValue = analogRead(A1);    
@@ -75,7 +219,8 @@ void loop(){
     int mediaTempFinal = mediaTemp / contadorMedia;
     int mediaUmidFinal = mediaUmid / contadorMedia;
     int mediaLumiFinal = mediaLumi / contadorMedia;   
-    
+
+    contTimer = 65;
     lcd.clear(); 
     lcd.setCursor(0,0);           
     lcd.print("Lumi"); 
@@ -149,3 +294,49 @@ String leString(int enderecoBase){
   }
   return mensagem; 
 }
+
+
+void dinoAnimacion() 
+{
+  lcd.createChar(1,chardino1);
+  lcd.createChar(2,chardino2);
+  lcd.createChar(3,chardino3);
+  lcd.createChar(4,chardino4);
+  lcd.createChar(5,chardino5);
+  lcd.createChar(6,chardino6);
+  lcd.createChar(7,chardino7);
+  
+  lcd.setCursor(0,0);
+  lcd.write(1);
+  lcd.write(2);
+  lcd.write(3);
+  lcd.write(4);
+
+  lcd.setCursor(0,1);
+  lcd.write(5);
+  lcd.write(6);
+  lcd.write(7);
+
+  lcd.createChar(6,charpieder1);
+  lcd.createChar(7,charpieder2);
+  lcd.setCursor(0,1);
+  lcd.write(5);
+  lcd.write(6);
+  lcd.write(7);
+
+  lcd.createChar(5,charpieiz1);
+  lcd.createChar(6,charpieiz2);
+  lcd.setCursor(0,1);
+  lcd.write(5);
+  lcd.write(6);
+  lcd.write(7);
+}
+
+ void letras()
+  {
+      lcd.clear();
+      lcd.setCursor(4,0);
+      lcd.print("Ardudino");
+      lcd.setCursor(4,1);
+      lcd.print("<<EC-5>>");   
+  }
